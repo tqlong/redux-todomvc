44ca101523811fa75be100d5db7b6582cd6d9b4d
Long <tqlong.uet@gmail.com>
2016-10-18 03:32:33 +0700

    After css

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack'); // Requiring the webpack lib
+
+module.exports = {
+  entry: [
+    './src/index.jsx'
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }, {
+      test: /\.css$/,
+      loader: 'style!css' // add the css loader
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin() // Wire in the hot loading plugin
+  ]
+};
